db46d40bf9187f74cf641248a3988dd6812c02f0
Ezejiofor Cosmas <ezejioforcosmas@gmail.com>
2026-02-15 18:01:48 +0100

    chore: update redis

--- src/queues/emailQueue.ts
+++ src/queues/emailQueue.ts
@@ -1,9 +1,11 @@
 import { Queue } from "bullmq";
 
-const connection = {
-  host: process.env.REDIS_HOST || "127.0.0.1",
-  port: parseInt(process.env.REDIS_PORT || "6379"),
-};
+const connection = process.env.REDIS_URL
+  ? { url: process.env.REDIS_URL }
+  : {
+      host: "127.0.0.1",
+      port: 6379,
+    };
 
 export const emailQueue = new Queue("email-queue", {
   connection,
